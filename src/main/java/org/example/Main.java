52e7242a5cdfaa605d21742983c617ab84716e77
faisalprady13 <faisalprady@gmail.com>
2026-04-28 14:22:11 +0200

    add sum feature

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,36 +4,38 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
-
     }
 
     static boolean isPositive(int a) {
-        return (a >=0);
+        return (a >= 0);
     }
 
     static Character getFirstLetter(String text) {
-        return text.isEmpty() ? null: text.charAt(0);
+        return text.isEmpty() ? null : text.charAt(0);
     }
 
-    static int divide(int a, int b){
-        if(b==0) throw new IllegalArgumentException("b must not be 0");
-        return a/b;
+    static int divide(int a, int b) {
+        if (b == 0) throw new IllegalArgumentException("b must not be 0");
+        return a / b;
     }
 
 
-
-    static boolean isPrime(int number){
+    static boolean isPrime(int number) {
         boolean result = true;
-        if(number>1) {
+        if (number > 1) {
             for (int i = 2; i < number - 1; i++) {
                 if (number % i == 0) {
                     result = false;
                     break;
                 }
             }
-        }else{
-            result=false;
+        } else {
+            result = false;
         }
         return result;
+    }
+
+    static int sum(int a, int b) {
+        return a + b;
     }
 }
--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -8,56 +8,62 @@ import org.junit.jupiter.params.provider.CsvSources;
 
 import static org.junit.jupiter.api.Assertions.*;
 
- class MainTest {
+class MainTest {
 
     @Test
-     void isPositive_shouldReturnTrue_whenGivenPositive() {
+    void isPositive_shouldReturnTrue_whenGivenPositive() {
         assertTrue(Main.isPositive(2));
     }
 
     @Test
-     void isPositive_shouldReturnFalse_whenGivenNegative() {
+    void isPositive_shouldReturnFalse_whenGivenNegative() {
         assertFalse(Main.isPositive(-2));
     }
 
     @Test
-     void getFirstLetter_shouldReturnT_whenGivenTest() {
-        assertEquals('T',Main.getFirstLetter("Test"));
+    void getFirstLetter_shouldReturnT_whenGivenTest() {
+        assertEquals('T', Main.getFirstLetter("Test"));
     }
 
     @Test
-     void getFirstLetter_shouldReturnNull_whenGivenEmptyString() {
+    void getFirstLetter_shouldReturnNull_whenGivenEmptyString() {
         assertNull(Main.getFirstLetter(""));
     }
+
     @Test
-     void divide_shouldReturn5_whenGiven10And2() {
-      assertEquals(5,Main.divide(10,2));
+    void divide_shouldReturn5_whenGiven10And2() {
+        assertEquals(5, Main.divide(10, 2));
 
     }
 
     @Test
-     void divide_shouldTrowException_whenGivenAnyNumberAnd0() {
-      Exception exception=  assertThrows(IllegalArgumentException.class,()->Main.divide(1,0));
+    void divide_shouldTrowException_whenGivenAnyNumberAnd0() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> Main.divide(1, 0));
 
         String expectedMessage = "b must not be 0";
         String actualMessage = exception.getMessage();
 
-        assertEquals(expectedMessage,actualMessage);
+        assertEquals(expectedMessage, actualMessage);
     }
 
     @ParameterizedTest
     @CsvSource({
-        "1,false",
-        "-2,false",
-        "-3,false",
-        "0,false",
-        "2,true",
-        "3,true",
-        "4,false",
-        "11,true",
+            "1,false",
+            "-2,false",
+            "-3,false",
+            "0,false",
+            "2,true",
+            "3,true",
+            "4,false",
+            "11,true",
     })
-     void isPrime(int number, boolean expected) {
-      assertEquals(expected,Main.isPrime(number));
+    void isPrime(int number, boolean expected) {
+        assertEquals(expected, Main.isPrime(number));
     }
 
+
+    @Test
+    void sum_shouldReturn5_whenGiven2and3() {
+        assertEquals(5, Main.sum(2, 3));
+    }
 }
